fb093e8c7c8f356c10a0c5d4d34507dca807d05e
Antonio Lisi <jack281291@users.noreply.github.com>
2021-01-19 17:15:32 +0100

    Update churn.sql

--- src/churn.sql
+++ src/churn.sql
@@ -1,14 +1,31 @@
-create table transactions_0 as select distinct user_id, DATE("2020-01-01") as date_now from transactions;
+CREATE table count_users as SELECT date, COUNT(distinct user_id) as n_users from transactions GROUP By date 
+
+CREATE TABLE dates_range as SELECT * FROM (
+WITH RECURSIVE dates(date) AS (
+  VALUES('2019-07-01')
+  UNION ALL
+  SELECT date(date, '+1 day')
+  FROM dates
+  WHERE date < '2020-06-30'
+)
+SELECT date as date_now FROM dates)
+
+/* Here I'm assuminig that we have all the transactions in one table and I'm taking all the user_id in order to evaluate their status for each day bettween 
+2019-07-01 and 2020-06-30 */
+CREATE table transactions_0 as 
+SELECT t1.user_id, t2.date_now from 
+((select distinct user_id, 1 as k from transactions) as t1 left JOIN (SELECT date_now, 1 as k from dates_range) as t2 on t1.k = t2.k)
+order by date_now;
 
 create table transactions_1 as select distinct t.*, t2.* from transactions_0 t left join transactions t2
-on t.user_id = t2.user_id where t.date_now >= t2.date_;
+on t.user_id = t2.user_id where t.date_now >= t2.date;
 
 select count(user_id) as n_user, date_now, status from (select t.*,
-       (case when date_now = min(date_) over (partition by user_id)
+       (case when date_now = min(date) over (partition by user_id)
              then 'New'
-             when date_now < date(lag(date_) over (partition by user_id order by date_), "+28 Days")
+             when date_now < date(lag(date) over (partition by user_id order by date), "+28 Days")
              then 'Active'
-             when date_now < date(lead(date_) over (partition by user_id order by date_),  "-28 Days")
+             when date_now < date(lead(date) over (partition by user_id order by date),  "-28 Days")
              then 'Churned'
              else 'Reactivated'
         end) as status
